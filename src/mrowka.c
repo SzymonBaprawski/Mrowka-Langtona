d69b084e3154e0eb87b038e53241915ad858a934
BartekM7 <153267249+BartekM7@users.noreply.github.com>
2024-01-18 17:48:04 +0000

    Add files via upload
    rysowanie i logika mrowki

---
+++ src/mrowka.c
@@ -0,0 +1,91 @@
+#include "mrowka.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void mrowkaStart(struct mrowka m, int a, int b, enum ways w){
+    m.x = a;
+    m.y =b;
+    m.way = w;
+}
+void moveMrowka(struct mrowka m, wchar_t **mapa){
+    if(mapa[m.x][m.y]==L'█'){
+        switch (m.way) {
+            case N:
+                m.way = W;
+                m.x--;
+                break;
+            case S:
+                m.way = E;
+                m.x++;
+                break;
+            case W:
+                m.way = S;
+                m.y++;
+                break;
+            case E:
+                m.way = N;
+                m.y--;
+                break;
+
+            
+        }
+    }else if(mapa[m.x][m.y]==L' '){
+        switch (m.way) {
+            case N:
+                m.way = E;
+                m.x++;
+                break;
+            case S:
+                m.way = W;
+                m.x--;
+                break;
+            case W:
+                m.way = S;
+                m.y++;
+                break;
+            case E:
+                m.way = N;
+                m.y--;
+                break;
+
+
+        }
+    }
+}
+void drawMrowka(struct mrowka m, wchar_t **mapa){
+    if(mapa[m.x][m.y]==L'█'){
+        switch (m.way) {
+            case N:
+                mapa[m.x][m.y]=L'▲';
+                break;
+            case S:
+                mapa[m.x][m.y]=L'▼';
+                break;
+            case W:
+                mapa[m.x][m.y]=L'◀';
+                break;
+            case E:
+                mapa[m.x][m.y]=L'▶';
+                break;
+
+
+        }
+    }else if(mapa[m.x][m.y]==L' '){
+        switch (m.way) {
+            case N:
+                mapa[m.x][m.y]=L'△';
+                break;
+            case S:
+                mapa[m.x][m.y]=L'▽';
+                break;
+            case W:
+                mapa[m.x][m.y]=L'◁';
+                break;
+            case E:
+                mapa[m.x][m.y]=L'▷';
+                break;
+
+        }
+
+    }
+}
---
+++ src/mrowka.h
@@ -0,0 +1,20 @@
+//
+// Created by Bartek on 1/18/2024.
+//
+
+#ifndef KOD_MROWKA_H
+#define KOD_MROWKA_H
+
+#include <stdio.h>
+
+enum ways {N,S,E,W};
+
+struct mrowka {
+    enum ways way;
+    int x,y;
+};
+void mrowkaStart(struct mrowka m, int a, int b, enum ways w);
+void moveMrowka(struct mrowka m, wchar_t **mapa);
+void drawMrowka(struct mrowka m, wchar_t **mapa);
+
+#endif //KOD_MROWKA_H
