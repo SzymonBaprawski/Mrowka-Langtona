332b68a545a4420f7dfaebc21dea8228634f98d8
BartekM7 <153267249+BartekM7@users.noreply.github.com>
2024-01-18 21:21:56 +0000

    Update wczytanie.c

--- src/wczytanie.c
+++ src/wczytanie.c
@@ -2,7 +2,7 @@
 #include "wczytanie.h"
 #include <stdio.h>
 
-int wczytanieMapy(char *nazwa, wchar_t mapa[MAX_ROWS][MAX_COLUMNS], int rows, int columns){
+int wczytanieMapy(char *nazwa, wchar_t mapa[MAX_ROWS][MAX_COLUMNS], int *rows, int *columns){
     FILE *f = fopen(nazwa, "r");
     if(f == NULL){
         printf("Nie udalo sie wczytac pliku");
@@ -10,8 +10,8 @@ int wczytanieMapy(char *nazwa, wchar_t mapa[MAX_ROWS][MAX_COLUMNS], int rows, in
     }
     fscanf(f,"%d,%d",rows,columns);
 
-    for(int i=0; i<rows;i++){
-        for(int j=0; j<columns; j++){
+    for(int i=0; i<*rows;i++){
+        for(int j=0; j<*columns; j++){
             if(fscanf(f,"%ls",&mapa[i][j])!=1){
                 fclose(f);
                 return -1;
