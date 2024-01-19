16210f3e66703738a4c9db1785e819d9534eec49
Szymon Baprawski <szymonbap@outlook.com>
2024-01-19 01:22:38 +0100

    wszystko działą i jest git :D :tada:

--- src/mrowka.c
+++ src/mrowka.c
@@ -4,64 +4,59 @@
 #include <wchar.h>
 
 int przesun_mrowke(wchar_t **mapa, int szerokosc, int wysokosc, int *x, int *y, char *kierunek){
-    
+    if (*x == 0 || *x == szerokosc-1 || *y == 0 || *y == wysokosc-1){
+        return 1;
+    }
     if (mapa[*y][*x] == L'█'){
         if (*kierunek == 'N'){
             *kierunek = 'E';
-        } else if (*kierunek == 'E'){
-            *kierunek = 'S';
-        } else if (*kierunek == 'S'){
-            *kierunek = 'W';
-        } else if (*kierunek == 'W'){
-            *kierunek = 'N';
+            mapa[*y][*x] = L' ';
+            *x += 1;
+            return 0;
         }
-        mapa[*y][*x] = L' ';
+        else if (*kierunek == 'E'){
+            *kierunek = 'S';
+            mapa[*y][*x] = L' ';
+            *y += 1;
+            return 0;
+        }
+        else if (*kierunek == 'S'){
+            *kierunek = 'W';
+            mapa[*y][*x] = L' ';
+            *x -= 1;
+            return 0;
+        }
+        else if (*kierunek == 'W'){
+            *kierunek = 'N';
+            mapa[*y][*x] = L' ';
+            *y -= 1;
+            return 0;
+        }
     }
-    else{
+    else if (mapa[*y][*x] == L' '){
         if (*kierunek == 'N'){
             *kierunek = 'W';
-        } else if (*kierunek == 'E'){
+            mapa[*y][*x] = L'█';
+            *x -= 1;
+            return 0;
+        }
+        else if (*kierunek == 'E'){
             *kierunek = 'N';
-        } else if (*kierunek == 'S'){
+            mapa[*y][*x] = L'█';
+            *y -= 1;
+            return 0;
+        }
+        else if (*kierunek == 'S'){
             *kierunek = 'E';
-        } else if (*kierunek == 'W'){
+            mapa[*y][*x] = L'█';
+            *x += 1;
+            return 0;
+        }
+        else if (*kierunek == 'W'){
             *kierunek = 'S';
-        }
-        mapa[*y][*x] = L'█';
-    }
-
-    if (*kierunek == 'N'){
-        if (*y == 0){
-            return 1;
-        }
-        else{
-            *y = *y - 1;
+            mapa[*y][*x] = L'█';
+            *y += 1;
+            return 0;
         }
     }
-    else if (*kierunek == 'E'){
-        if (*x == szerokosc - 1){
-            return 1;
-        }
-        else{
-            *x = *x + 1;
-        }
-    }
-    else if (*kierunek == 'S'){
-        if (*y == wysokosc - 1){
-            return 1;
-        }
-        else{
-            *y = *y + 1;
-        }
-    }
-    else if (*kierunek == 'W'){
-        if (*x == 0){
-            return 1;
-        }
-        else{
-            *x = *x - 1;
-        }
-    }
-    
-    return 0;
 }
